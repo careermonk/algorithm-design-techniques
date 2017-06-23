4b57dc97ed5c37c219532ad298a848c8c7e9aae8
CareerMonk <careermonk@users.noreply.github.com>
2017-06-23 12:11:44 +0530

    Update max_sum_with_no_two_contiguous_numbers.py

--- max_sum_with_no_two_contiguous_numbers.py
+++ max_sum_with_no_two_contiguous_numbers.py
@@ -2,14 +2,14 @@ def max_sum_with_no_two_contiguous_numbers(A):
 	n = len(A)
 	M = [0] * (n+1)
 	M[0] = A[0] 
-	M[0] = max (A[0], A[1])
+	M[1] = max (A[0], A[1])
 
 	for i in range(2, n): 
-		if( M[i-1]>M[i-2]+A[i]):
+		if( M[i-1] > M[i-2] + A[i]):
 			M[i] = M[i-1]
 		else: 	
-			M[i] = M[i-2]+A[i]	
-
+			M[i] = M[i-2] + A[i]	
+	print M
 	return M[n-1]
 
 A = [-2, 3, -16, 100, -4, 5]
