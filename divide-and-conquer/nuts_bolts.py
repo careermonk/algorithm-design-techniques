96faa4c3ea7b19617e2ce46548b8de61f69df0bc
CareerMonk <careermonk@users.noreply.github.com>
2017-06-26 22:59:13 +0530

    Update nuts_bolts.py

--- divide-and-conquer/nuts_bolts.py
+++ divide-and-conquer/nuts_bolts.py
@@ -1,3 +1,13 @@
+# Copyright (c) June 02, 2017 CareerMonk Publications and others.
+# E-Mail           		: info@careermonk.com 
+# Creation Date    		: 2017-06-02 06:15:46 
+# Last modification		: 2017-06-02 
+# Modified by		        : Narasimha Karumanchi 
+# Book Title			: Algorithm Design Techniques
+# Warranty         		: This software is provided "as is" without any 
+# 				 warranty; without even the implied warranty of 
+# 				 merchantability or fitness for a particular purpose. 
+
 def partition( A, pivot, start, end):
 	i = start  # save for the counterpart's pivot
 	for j in range(start, end):
