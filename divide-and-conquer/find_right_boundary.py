1b319f71124051bfef641e198ff15a88d6ce4899
CareerMonk <careermonk@users.noreply.github.com>
2017-06-26 22:59:37 +0530

    Update find_right_boundary.py

--- divide-and-conquer/find_right_boundary.py
+++ divide-and-conquer/find_right_boundary.py
@@ -1,3 +1,13 @@
+# Copyright (c) June 02, 2017 CareerMonk Publications and others.
+# E-Mail           		: info@careermonk.com 
+# Creation Date    		: 2017-06-02 06:15:46 
+# Last modification		: 2017-06-02 
+# Modified by		        : Narasimha Karumanchi 
+# Book Title			: Algorithm Design Techniques
+# Warranty         		: This software is provided "as is" without any 
+# 				 warranty; without even the implied warranty of 
+# 				 merchantability or fitness for a particular purpose. 
+
 def find_right_boundary(A, left, right, X):
 	if left < right:
 		mid = (left + right) // 2
