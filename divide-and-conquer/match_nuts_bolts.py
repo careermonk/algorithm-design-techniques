0beb8ef41572a89468b2c4f68c9bda15cefd65a9
CareerMonk <careermonk@users.noreply.github.com>
2017-06-26 22:49:41 +0530

    Create match_nuts_bolts.py

---
+++ divide-and-conquer/match_nuts_bolts.py
@@ -0,0 +1,32 @@
+def partition( A, pivot, start, end):
+	i = start  # save for the counterpart's pivot
+	for j in range(start, end):
+		if A[j] < pivot:
+			A[i], A[j] = A[j], A[i]
+			i = i + 1
+		elif A[j] == pivot:
+			A[j], A[end] = A[end], A[j]
+			j = j - 1
+
+	# move the counterpart's pivot from start to left
+	A[i], A[end] = A[end], A[i]
+
+	return i
+
+def match_nuts_bolts( nuts, bolts, start, end):
+
+	if start >= end:
+		return
+
+	b_pivot= end #random(start, end+1)
+	n_pivot = partition(nuts, bolts[b_pivot], start, end)
+	partition(bolts, nuts[n_pivot], start, end)
+	match_nuts_bolts(nuts, bolts, start, n_pivot)
+	match_nuts_bolts(nuts, bolts, n_pivot+1, end)
+
+	    
+nuts = ['@', '#', '$', '%', '^', '&']
+bolts = ['$', '%', '&', '^', '@', '#']
+assert len(nuts) == len(bolts)
+match_nuts_bolts(nuts, bolts, 0, len(bolts)-1)
+print nuts, bolts
