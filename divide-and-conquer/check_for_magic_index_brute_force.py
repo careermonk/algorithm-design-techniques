8627f90b6c3ff6f80e380c8da224def791a18782
CareerMonk <careermonk@users.noreply.github.com>
2017-06-28 21:24:11 +0530

    Update and rename check_for_Ai_i_brute_force.py to check_for_magic_index_brute_force.py

--- divide-and-conquer/check_for_magic_index_brute_force.py
+++ divide-and-conquer/check_for_magic_index_brute_force.py
@@ -1,8 +1,8 @@
-def check_for_Ai_i(A):
+def check_for_magic_index(A):
 	for i in range(len(A)):
 		if(A[i] == i) :
 			return i
 	return None
 
 A = [-1, 0, 2, 5, 7, 9, 11, 12, 19]
-print check_for_Ai_i(A)
+print check_for_magic_index(A)
