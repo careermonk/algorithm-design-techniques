1b48a966d3e91f0d142cbc941936baf71e0a151b
CareerMonk <careermonk@users.noreply.github.com>
2017-07-02 20:05:46 +0530

    Update towers_of_hanoi.py

--- recursion/towers_of_hanoi.py
+++ recursion/towers_of_hanoi.py
@@ -1,8 +1,8 @@
-def move_tower(number_of_disks,fromTower, toTower, withTower):
-    if number_of_disks >= 1:
-        move_tower(number_of_disks-1,fromTower,withTower,toTower)
+def move_tower(numberOfDisks,fromTower, toTower, withTower):
+    if numberOfDisks >= 1:
+        move_tower(numberOfDisks-1,fromTower,withTower,toTower)
         move_disk(fromTower,toTower)
-        move_tower(number_of_disks-1,withTower,toTower,fromTower)
+        move_tower(numberOfDisks-1,withTower,toTower,fromTower)
 
 def move_disk(fromTower,toTower):
     print("Moving disk from ",fromTower," to ",toTower)
