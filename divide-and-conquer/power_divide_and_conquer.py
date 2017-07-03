77e1cced4b707788859fb9aabc57ff95f6742f1c
CareerMonk <careermonk@users.noreply.github.com>
2017-07-03 13:12:01 +0530

    Update power_divide_and_conquer.py

--- divide-and-conquer/power_divide_and_conquer.py
+++ divide-and-conquer/power_divide_and_conquer.py
@@ -1,15 +1,20 @@
 import math
 def power_divide_and_conquer(a, n):
-    """Divide and Conquer power algorithm"""
-    if n == 0: 
-    	return 1
+	"""Divide and Conquer power algorithm"""
+
+	# base case
+	if n == 0: 
+		return 1
+
+	# base case
 	if a == 0:
 		return 0
-    x = power_divide_and_conquer(a, math.floor(n/2))
-    if n % 2 == 0: 
-    	return x * x
-    else: return a * x * x
 
+	x = power_divide_and_conquer(a, math.floor(n/2))
+	if n % 2 == 0: 
+		return x * x
+	else: 
+		return a * x * x
 
 print power_divide_and_conquer(2, 4)
 print power_divide_and_conquer(2, 3)
