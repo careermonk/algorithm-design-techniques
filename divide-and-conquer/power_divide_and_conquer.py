c432fc7cd6ce5beecf6e2db5b597a43bd436bf53
CareerMonk <careermonk@users.noreply.github.com>
2017-07-03 13:08:01 +0530

    Create power_divide_and_conquer.py

---
+++ divide-and-conquer/power_divide_and_conquer.py
@@ -0,0 +1,15 @@
+import math
+def power_divide_and_conquer(a, n):
+    """Divide and Conquer power algorithm"""
+    if n == 0: 
+    	return 1
+	if a == 0:
+		return 0
+    x = power_divide_and_conquer(a, math.floor(n/2))
+    if n % 2 == 0: 
+    	return x * x
+    else: return a * x * x
+
+
+print power_divide_and_conquer(2, 4)
+print power_divide_and_conquer(2, 3)
