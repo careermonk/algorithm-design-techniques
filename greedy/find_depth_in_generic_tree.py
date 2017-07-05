b283b79bfb24623c78ac931f7b1df2eb79973fbe
CareerMonk <careermonk@users.noreply.github.com>
2017-07-05 18:31:45 +0530

    Create find_depth_in_generic_tree.py

---
+++ greedy/find_depth_in_generic_tree.py
@@ -0,0 +1,16 @@
+def find_depth_in_generic_tree(parent):
+	max_depth =-1
+	current_depth =-1
+	for i in range (0, len(parent)):
+		current_depth = 0
+		j = i
+		while(parent[j] != -1):
+		       current_depth += 1
+		       j = parent[j]
+		if(current_depth > max_depth):
+		       max_depth = current_depth
+	
+	return max_depth
+	
+parent=[-1, 0, 1, 6, 6, 0, 0, 2, 7]
+print "Depth of given generic tree is:", find_depth_in_generic_tree(parent)
