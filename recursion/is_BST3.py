ecf615689e51c84688cc9bda8cadde8d66f47e20
CareerMonk <careermonk@users.noreply.github.com>
2017-07-07 12:32:09 +0530

    Create is_BST3.py

---
+++ recursion/is_BST3.py
@@ -0,0 +1,48 @@
+'''Binary Search Tree Node'''
+class BSTNode:
+    def __init__(root, data):
+        root.left = None
+        root.right = None
+        root.data = data
+
+def find_min(root):
+	current = root
+	if current is None:  
+		return None
+	while current.left is not None:
+		current = current.left
+	return current
+
+def find_max(root):
+	current = root
+	if current is None:  
+		return None
+	while current.right is not None:
+		current = current.right
+	return current
+
+
+def is_BST(root, min, max):
+	if root is None:
+		return True
+	if root.data<=min or root.data>=max:
+		return False
+	result = is_BST(root.left, min, root.data)
+	result = result and is_BST(root.right, root.data, max)
+	return result
+
+# create BST    
+node1, node2, node3, node4, node5 = \
+BSTNode(6), BSTNode(2), BSTNode(8), BSTNode(1), BSTNode(9)
+node1.left, node1.right = node2, node3
+node2.left, node2.right = node4, node5
+root = node1
+print is_BST(root, float("-infinity"), float("infinity"))  
+
+# create BST    
+node1, node2, node3, node4, node5 = \
+BSTNode(9), BSTNode(2), BSTNode(10), BSTNode(1), BSTNode(6)
+node1.left, node1.right = node2, node3
+node2.left, node2.right = node4, node5
+root = node1
+print is_BST(root, float("-infinity"), float("infinity"))  
