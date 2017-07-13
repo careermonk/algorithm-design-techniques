553df430a050c7d1a54593332b9641914dd5b10f
CareerMonk <careermonk@users.noreply.github.com>
2017-07-13 08:12:50 +0530

    Update Vertex.py

--- greedy/Vertex.py
+++ greedy/Vertex.py
@@ -1,40 +1,33 @@
 class Vertex:
-    def __init__(self, node):
-        self.id = node
-        self.adjacent = {}
-
-        # Set distance to infinity for all nodes
-        self.distance = sys.maxint
- 
-        # Mark all nodes unvisited        
-        self.visited = False  
-        
-        # Predecessor
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
         self.previous = None
-
-    def add_neighbor(self, neighbor, weight=0):
-        self.adjacent[neighbor] = weight
-
-    def get_connections(self):
-        return self.adjacent.keys()  
-
-    def get_vertex_ID(self):
-        return self.id
-
-    def get_weight(self, neighbor):
-        return self.adjacent[neighbor]
-
-    def set_distance(self, dist):
-        self.distance = dist
-
-    def get_distance(self):
-        return self.distance
+        self.distance = None
 
     def set_previous(self, prev):
         self.previous = prev
 
-    def set_visited(self):
-        self.visited = True
+    def get_previous(self):
+        return self.previous
+
+    def set_distance(self, d):
+        self.distance = d
+
+    def get_distance(self):
+        return self.distance
+
+    def add_neighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
 
     def __str__(self):
-        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
+        return str(self.id) + " is connected to: " + str([x.id for x in self.connectedTo])
+
+    def get_connections(self):
+        return self.connectedTo.keys()
+
+    def get_vertex_ID(self):
+        return self.id
+
+    def get_weight(self, nbr):
+        return self.connectedTo[nbr]
