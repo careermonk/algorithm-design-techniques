19c8c92642d5c3be9684ee28254b2398baba69b2
CareerMonk <careermonk@users.noreply.github.com>
2017-07-14 15:08:40 +0530

    Update min_refill_gas_stops.py

--- greedy/min_refill_gas_stops.py
+++ greedy/min_refill_gas_stops.py
@@ -1,26 +1,25 @@
-def min_refill_gas_stops(stationDist, carMaxDistance):
+def min_refill_gas_stops(stationDist, maxDistance):
     curPos = 0
     minRefill = 0
-    curDistance = carMaxDistance
+    curDistance = maxDistance
     numRefillStations = len(stationDist)
-    
+    stops = []
     while(curPos<numRefillStations):
         while(curPos < numRefillStations and stationDist[curPos] <= curDistance):
             curPos += 1
 
         if (curPos >= numRefillStations):
-            return minRefill
+            return minRefill, stops
 
         curPos -= 1
         minRefill += 1
-        curDistance = stationDist[curPos] + carMaxDistance
-     
+        curDistance = stationDist[curPos] + maxDistance
 
-    if (stationDist[curPos] < curDistance):
-        minRefill += 1
+        if (stationDist[curPos] < curDistance):
+            minRefill += 1
+            stops.append(curPos)
     
-    return minRefill
-   
+    return minRefill, stops
 
 # Test Cases
 print "Minimum refills required for reaching destination:", min_refill_gas_stops([0, 20, 37, 55, 75, 95], 40)
