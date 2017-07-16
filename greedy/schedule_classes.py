4830feb408fd62a3d014c5c865c589927a2596fd
CareerMonk <careermonk@users.noreply.github.com>
2017-07-16 13:50:42 +0530

    Update schedule_classes.py

--- greedy/schedule_classes.py
+++ greedy/schedule_classes.py
@@ -8,23 +8,22 @@ class ClassTimings(object):
 
 def schedule_classes(I):
     I.sort(lambda x, y: x.finish - y.finish) 
-
-    X = []
+    count = 1
     finish = 0
     for i in I:
         if finish <= i.start:
             finish = i.finish
-            X.append(i)
-
-    return X
+        else:
+            count = count + 1
+    return count
 
 if __name__ == '__main__':
     I = []
     I.append(ClassTimings(1, 6))
     I.append(ClassTimings(1, 2))
     I.append(ClassTimings(3, 4))
+    I.append(ClassTimings(5, 8))
     I.append(ClassTimings(5, 7))
-    I.append(ClassTimings(5, 7))
+    I.append(ClassTimings(6, 8))
     I.append(ClassTimings(8, 9))
-    X = schedule_classes(I)
-print "New room for classes",X, "and need total of ", len(X), "rooms"
+print "Classrooms needed",schedule_classes(I)
