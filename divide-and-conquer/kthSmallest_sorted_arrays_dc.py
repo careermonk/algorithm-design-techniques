a1c02cfb91137a8c89879f3bacd5a90963402bbc
CareerMonk <careermonk@users.noreply.github.com>
2017-07-21 15:39:18 +0530

    Update kthSmallest_sorted_arrays_dc.py

--- divide-and-conquer/kthSmallest_sorted_arrays_dc.py
+++ divide-and-conquer/kthSmallest_sorted_arrays_dc.py
@@ -1,4 +1,4 @@
-def kthSmAllest(A, B, k):
+def kthSmallest(A, B, k):
     m = len(A); n= len(B)
     if k >= m + n:
         return -1
@@ -11,27 +11,27 @@ def kthSmAllest(A, B, k):
     j = n/2
     if i+j<k:
         if A[i]>B[j]:
-            return kthSmAllest(A, B[j+1:], k-j-1)
+            return kthSmallest(A, B[j+1:], k-j-1)
         else:
-            return kthSmAllest(A[i+1:], B, k-i-1)
+            return kthSmallest(A[i+1:], B, k-i-1)
     else:
         if A[i]>B[j]:
-            return kthSmAllest(A[:i], B, k)
+            return kthSmallest(A[:i], B, k)
         else:
-            return kthSmAllest(A, B[:j], k)
+            return kthSmallest(A, B[:j], k)
 	
 A = [3, 4, 29, 41, 45, 49, 79, 89]
 B = [1, 5, 8, 10, 50]
-print kthSmAllest(A, B, 1)
-print kthSmAllest(A, B, 2)
-print kthSmAllest(A, B, 3)
-print kthSmAllest(A, B, 4)
-print kthSmAllest(A, B, 5)
-print kthSmAllest(A, B, 6)
-print kthSmAllest(A, B, 7)
-print kthSmAllest(A, B, 8)
-print kthSmAllest(A, B, 9)
-print kthSmAllest(A, B, 10)
-print kthSmAllest(A, B, 11)
-print kthSmAllest(A, B, 12)
-print kthSmAllest(A, B, 13)
+print kthSmallest(A, B, 5)
+print kthSmallest(A, B, 2)
+print kthSmallest(A, B, 3)
+print kthSmallest(A, B, 4)
+print kthSmallest(A, B, 5)
+print kthSmallest(A, B, 6)
+print kthSmallest(A, B, 7)
+print kthSmallest(A, B, 8)
+print kthSmallest(A, B, 9)
+print kthSmallest(A, B, 10)
+print kthSmallest(A, B, 11)
+print kthSmallest(A, B, 12)
+print kthSmallest(A, B, 13)
