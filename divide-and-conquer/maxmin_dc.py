f9c94dc98ae2f9b118a6e833741f322d5132601c
CareerMonk <careermonk@users.noreply.github.com>
2017-07-21 19:08:32 +0530

    Update maxmin_dc.py

--- divide-and-conquer/maxmin_dc.py
+++ divide-and-conquer/maxmin_dc.py
@@ -10,8 +10,17 @@ def maxmin(A):
 	else:
 		min_left, max_left = maxmin(A[:n/2])
 		min_right, max_right = maxmin(A[n/2:])
-		minimum = min(min_left, min_right)
-		maximum = max(max_left, max_right)
+
+		if (min_left < min_right):
+			minimum = min_left
+		else:
+			minimum = min_right
+
+		if (max_left < max_right):
+			maximum = max_right
+		else:
+			maximum = max_left
+
 		return (minimum, maximum)
 	
 print maxmin([3, 42, 29, 1, 45, 9, 69, 19])
