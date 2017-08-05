93d1c138755e0c8d05b5d0af3a83994a35331c75
CareerMonk <careermonk@users.noreply.github.com>
2017-08-05 15:22:24 +0530

    Create min_heap.py

---
+++ greedy/min_heap.py
@@ -0,0 +1,61 @@
+class MinHeap:
+	def __init__(self):
+		self.A = [0]
+		self.size = 0
+
+	def percolate_up(self,i):
+		while i // 2 > 0:
+			if self.A[i] < self.A[i // 2]:
+				tmp = self.A[i // 2]
+				self.A[i // 2] = self.A[i]
+				self.A[i] = tmp
+			i = i // 2
+
+	def insert(self,k):
+		self.A.append(k)
+		self.size = self.size + 1
+		self.percolate_up(self.size)
+
+	def percolate_down(self,i):
+		while (i * 2) <= self.size:
+			minChild = self.min_child(i)
+			if self.A[i] > self.A[minChild]:
+				tmp = self.A[i]
+				self.A[i] = self.A[minChild]
+				self.A[minChild] = tmp
+			i = minChild
+
+	def min_child(self,i):
+		if i * 2 + 1 > self.size:
+			return i * 2
+		else:
+			if self.A[i*2] < self.A[i*2+1]:
+				return i * 2
+			else:
+				return i * 2 + 1
+
+	def delete_min(self):
+		retval = self.A[1]
+		self.A[1] = self.A[self.size]
+		self.size = self.size - 1
+		self.A.pop()
+		self.percolate_down(1)
+		return retval
+
+	def build_heap(self, A):
+		i = len(A) // 2
+		self.size = len(A)
+		self.A = [0] + A[:]
+		while (i > 0):
+			self.percolate_down(i)
+			i = i - 1
+
+	def k_smallest(self, k):
+		result = []
+		for i in range(k):
+			result.append(self.delete_min())
+		return result
+
+h = MinHeap()
+h.build_heap([10, 5, 1, 6, 20, 19, 22, 29, 32, 29, 4])
+print h.k_smallest(3)
