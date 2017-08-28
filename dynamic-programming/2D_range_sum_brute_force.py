9da38a1790d920078fcae451087c9ff7ea6f6f90
CareerMonk <careermonk@users.noreply.github.com>
2017-08-28 10:59:17 +0530

    Update 2D_range_sum_brute_force.py

--- dynamic-programming/2D_range_sum_brute_force.py
+++ dynamic-programming/2D_range_sum_brute_force.py
@@ -1,3 +1,13 @@
+# Copyright (c) June 02, 2017 CareerMonk Publications and others.
+# E-Mail           		: info@careermonk.com
+# Creation Date    		: 2017-06-02 06:15:46
+# Last modification		: 2017-06-02
+# Modified by		        : Narasimha Karumanchi
+# Book Title			: Algorithm Design Techniques
+# Warranty         		: This software is provided "as is" without any
+# 				 warranty; without even the implied warranty of
+# 				 merchantability or fitness for a particular purpose.
+
 def range_sum(matrix, row1, col1, row2, col2):
 	toal_sum = 0
 	for i in range(row1, row2 + 1):
